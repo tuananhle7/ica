498acc5697e05f791b0b6b15584930261703f5ce
Tuan Anh Le <letuananh120@gmail.com>
2021-08-22 21:07:18 -0400

    timeseries works

--- ica.py
+++ ica.py
@@ -180,6 +180,48 @@ def generate_signal(key, num_samples):
     return true_source, mixing_matrix, signal
 
 
+def generate_timeseries_signal(key, num_timesteps=500):
+    """Create a timeseries signal that's used in Aapo's tutorial.
+
+    Based on
+    https://github.com/probml/pyprobml/blob/master/scripts/ica_demo.py#L23
+
+    Args
+        key
+        num_timesteps
+    
+    Returns
+        true_source [num_timesteps, 4]
+        mixing_matrix [4, 4]
+        signal [num_timesteps, 4]
+    """
+    timesteps = jnp.arange(0, num_timesteps)
+    dim = 4
+    true_source = jnp.zeros((num_timesteps, dim))
+
+    true_source = true_source.at[:, 0].set(jnp.sin(timesteps / 2))  # sinusoid
+    true_source = true_source.at[:, 1].set(((timesteps % 23 - 11) / 9) ** 5)
+    true_source = true_source.at[:, 2].set((timesteps % 27 - 13) / 9)  # sawtooth
+
+    key, subkey = jax.random.split(key)
+    rand = jax.random.uniform(subkey, (num_timesteps,))
+    key, subkey = jax.random.split(key)
+    true_source = true_source.at[:, 3].set(
+        jnp.where(rand < 0.5, rand * 2 - 1, -1)
+        * jnp.log(jax.random.uniform(subkey, (num_timesteps,)))
+    )  # impulsive noise
+
+    true_source /= true_source.std(axis=0)
+    true_source -= true_source.mean(axis=0)
+    key, subkey = jax.random.split(key)
+    mixing_matrix = jax.random.uniform(subkey, (dim, dim))  # mixing matrix
+    return (
+        true_source,
+        mixing_matrix,
+        jax.vmap(get_signal, (None, 0), 0)(mixing_matrix, true_source),
+    )
+
+
 def preprocess_signal(signal):
     """Center and whiten the signal
     x_preprocessed = A @ (x - mean)
@@ -245,20 +287,39 @@ def ica(key, signal, get_source_log_prob, num_iterations=1000, lr=1e-3):
     return total_log_likelihoods, raw_mixing_matrices, preprocessing_params
 
 
+def plot_timeseries_signal(axs, signal):
+    """
+    Args
+        axs (list of length 4)
+        signal [num_timesteps, 4]
+    """
+    num_timesteps, dim = signal.shape
+    for i in range(dim):
+        axs[i].plot(signal[:, i], color="black", linewidth=0.7)
+
+
 def main():
     num_iterations = 500
     num_samples = 1000
     lr = 1e-3
 
     # Generate signal
+    # key = jax.random.PRNGKey(0)
+    # key, subkey = jax.random.split(key)
+    # true_source, mixing_matrix, signal = generate_signal(subkey, num_samples)
+
+    # Generate timeseries signal
     key = jax.random.PRNGKey(0)
     key, subkey = jax.random.split(key)
-    true_source, mixing_matrix, signal = generate_signal(subkey, num_samples)
+    true_source, mixing_matrix, signal = generate_timeseries_signal(subkey)
 
     # Run ICA
     key, subkey = jax.random.split(key)
+    # total_log_likelihoods, raw_mixing_matrices, preprocessing_params = ica(
+    #     key, signal, get_subgaussian_log_prob, num_iterations=num_iterations, lr=lr
+    # )
     total_log_likelihoods, raw_mixing_matrices, preprocessing_params = ica(
-        key, signal, get_subgaussian_log_prob, num_iterations=num_iterations, lr=lr
+        key, signal, get_supergaussian_log_prob, num_iterations=num_iterations, lr=lr
     )
 
     # Plot likelihoods
@@ -273,29 +334,61 @@ def main():
     print(f"True mixing matrix: {mixing_matrix}")
 
     # Plot training progress
-    # --Plot pngs
-    scatter_kwargs = {"s": 0.5, "c": "black", "marker": "o"}
+    # # --Plot pngs (simple)
+    # scatter_kwargs = {"s": 0.5, "c": "black", "marker": "o"}
+    # img_paths = []
+    # for iteration in np.linspace(0, num_iterations, 11):
+    #     source = jax.vmap(get_source, (0, None), 0)(
+    #         signal_preprocessed, raw_mixing_matrices[int(iteration)]
+    #     )
+
+    #     fig, axss = plt.subplots(2, 2, sharex=True, sharey=True)
+    #     axss[0, 0].scatter(true_source[:, 0], true_source[:, 1], **scatter_kwargs)
+    #     axss[0, 0].set_title("True source")
+    #     axss[0, 1].scatter(signal[:, 0], signal[:, 1], **scatter_kwargs)
+    #     axss[0, 1].set_title("Observed signal")
+    #     axss[1, 0].scatter(signal_preprocessed[:, 0], signal_preprocessed[:, 1], **scatter_kwargs)
+    #     axss[1, 0].set_title("PCA of observed signal")
+    #     axss[1, 1].scatter(source[:, 0], source[:, 1], **scatter_kwargs)
+    #     axss[1, 1].set_title("Recovered source")
+
+    #     # Formatting
+    #     for ax in axss.flat:
+    #         ax.set_xlim(-5, 5)
+    #         ax.set_ylim(-5, 5)
+    #         ax.tick_params(direction="in")
+
+    #     # Title
+    #     fig.suptitle(f"Iteration {int(iteration)}")
+
+    #     # Save
+    #     img_path = f"save/ica/{int(iteration)}.png"
+    #     util.save_fig(
+    #         fig, img_path, dpi=400, tight_layout_kwargs={"rect": [0, 0, 1, 1.02]},
+    #     )
+    #     img_paths.append(img_path)
+
+    # --Plot pngs (timeseries)
     img_paths = []
     for iteration in np.linspace(0, num_iterations, 11):
         source = jax.vmap(get_source, (0, None), 0)(
             signal_preprocessed, raw_mixing_matrices[int(iteration)]
         )
 
-        fig, axss = plt.subplots(2, 2, sharex=True, sharey=True)
-        axss[0, 0].scatter(true_source[:, 0], true_source[:, 1], **scatter_kwargs)
+        fig, axss = plt.subplots(8, 2, sharex=True, sharey=True, figsize=(12, 12))
+        plot_timeseries_signal(axss[:4, 0], true_source)
         axss[0, 0].set_title("True source")
-        axss[0, 1].scatter(signal[:, 0], signal[:, 1], **scatter_kwargs)
+        plot_timeseries_signal(axss[:4, 1], signal)
         axss[0, 1].set_title("Observed signal")
-        axss[1, 0].scatter(signal_preprocessed[:, 0], signal_preprocessed[:, 1], **scatter_kwargs)
-        axss[1, 0].set_title("PCA of observed signal")
-        axss[1, 1].scatter(source[:, 0], source[:, 1], **scatter_kwargs)
-        axss[1, 1].set_title("Recovered source")
+        plot_timeseries_signal(axss[4:, 0], signal_preprocessed)
+        axss[4, 0].set_title("PCA of observed signal")
+        plot_timeseries_signal(axss[4:, 1], source)
+        axss[4, 1].set_title("Recovered source")
 
         # Formatting
         for ax in axss.flat:
-            ax.set_xlim(-5, 5)
-            ax.set_ylim(-5, 5)
             ax.tick_params(direction="in")
+            ax.set_ylim(-2.5, 2.5)
 
         # Title
         fig.suptitle(f"Iteration {int(iteration)}")
@@ -303,7 +396,7 @@ def main():
         # Save
         img_path = f"save/ica/{int(iteration)}.png"
         util.save_fig(
-            fig, img_path, dpi=400, tight_layout_kwargs={"rect": [0, 0, 1, 1.02]},
+            fig, img_path, dpi=400, tight_layout_kwargs={"rect": [0, 0, 1, 1.0]},
         )
         img_paths.append(img_path)
 
